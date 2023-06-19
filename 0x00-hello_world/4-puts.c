8bd096eceaabbcfeebcea70683d3c785842bdac0
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-19 07:41:16 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ /**
+  * main - main function of the code 
+  */
+int main(void) 
+	/* in this case the put function replaces printf function
+	 */
+{
+       	puts("Programming is like building a multilingual puzzle");
+       return(0) ; 
+}       
