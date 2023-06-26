b2163e6ecf3ca2a1f751b5a32c9646af8c1dc53f
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-26 16:18:50 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,21 +3,22 @@
 /**
  * print_rev - Prints a string in reverse
  * @s: The string to print
- * return: void
+ *
+ * Return: void
  */
-
 void print_rev(char *s)
-
 {
 	int c = 0;
+
 	while (s[c] != '\0')
 	{
-	c++;
+		c++;
 	}
-	
-	for (c -= 1; c >=0; c--)
+
+	for (c -= 1; c >= 0; c--)
 	{
-	putchar(S[c]);
+		_putchar(s[c]);
 	}
-	putchar('\n');
+
+	_putchar('\n');
 }
