d0916aa343f514eb0263d9bd9f2a3b0d77de23c2
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-26 18:46:15 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,14 +2,14 @@
 
 /**
  * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
+ * @s: The pointer to convert.
  *
- * Return: An integer
+ * Return: An integer.
  */
 int _atoi(char *s)
 {
 	int c = 0;
-	int ni = 0;
+	unsigned int ni = 0;
 	int min = 1;
 	int isi = 0;
 
@@ -22,6 +22,14 @@ int _atoi(char *s)
 		while (s[c] >= '0' && s[c] <= '9')
 		{
 			isi = 1;
+			if ((ni > INT_MAX / 10) || (ni == INT_MAX / 10 && s[c] - '0' > INT_MAX % 10))
+			{
+				/* Handle overflow */
+				if (min == 1)
+					return INT_MAX;
+				else
+					return INT_MIN;
+			}
 			ni = (ni * 10) + (s[c] - '0');
 			c++;
 		}
@@ -29,7 +37,7 @@ int _atoi(char *s)
 		{
 			break;
 		}
-		c++;
 	}
+
 	return (ni * min);
 }
