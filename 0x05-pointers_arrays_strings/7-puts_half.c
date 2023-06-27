8cfb8a8839de594eaaab08cddcb95b3d803b0256
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-26 17:13:07 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,32 +3,33 @@
 /**
  * puts_half - Prints half of a string
  * @str: The string to print
- * return: void
+ *
+ * Return: void
  */
-
 void puts_half(char *str)
-
 {
 	int j = 0;
 	int k;
 
 	while (str[j] != '\0')
 	{
-	j++;
+		j++;
 	}
+
 	if (j % 2 == 1)
 	{
-	k = (j - 1) / 2;
-	k += 1;
+		k = (j - 1) / 2;
+		k += 1;
 	}
 	else
 	{
-	k = j / 2;
+		k = j / 2;
 	}
 
 	for (; k < j; k++)
 	{
-	_putchar(strl[k]);
+		_putchar(str[k]);
 	}
+
 	_putchar('\n');
 }
