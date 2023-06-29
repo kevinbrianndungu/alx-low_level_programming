ac8f528999c41c2b16a2b1b2c898e8c91cdd8478
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 15:58:29 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,26 +2,26 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	while (s[i] == ' ')
-		i++;
+    while (s[i] == ' ')
+        i++;
 
-	if (s[i] == '-' || s[i] == '+') {
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    if (s[i] == '-' || s[i] == '+') {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	while (s[i] >= '0' && s[i] <= '9') {
-		if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-			return (sign == 1 ? INT_MAX : INT_MIN);
+    while (s[i] >= '0' && s[i] <= '9') {
+        if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+            return (sign == 1 ? INT_MAX : INT_MIN);
 
-		result = (result * 10) + (s[i] - '0');
-		i++;
-	}
+        result = (result * 10) + (s[i] - '0');
+        i++;
+    }
 
-	return (result * sign);
+    return (result * sign);
 }
