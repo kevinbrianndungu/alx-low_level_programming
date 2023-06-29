d77a37829bcc6d6e81db3ffc0b8846da00ac60f1
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 10:49:01 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase
+ * @str: The string to modify
+ *
+ * Return: The modified string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (islower(str[i]))
+			str[i] = toupper(str[i]);
+		i++;
+	}
+
+	return (str);
+}
