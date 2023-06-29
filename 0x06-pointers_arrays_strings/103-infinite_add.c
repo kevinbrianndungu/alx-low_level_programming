24e1417feeaf1ce1f0ebe3bde8c83033f34635e1
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 14:24:40 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,7 +19,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (j = 0; n2[j] != '\0'; j++)
 		;
 
-	if (i > size_r || j > size_r)
+	if (i >= size_r || j >= size_r)
 		return (0);
 
 	r[size_r - 1] = '\0';
