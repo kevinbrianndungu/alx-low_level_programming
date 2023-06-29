bf200804443dd87356130764e0b70e29363b97ac
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 15:53:52 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,7 +16,7 @@ int _atoi(char *s)
 	}
 
 	while (s[i] >= '0' && s[i] <= '9') {
-		if ((result * 10) + (s[i] - '0') < result)
+		if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
 			return (sign == 1 ? INT_MAX : INT_MIN);
 
 		result = (result * 10) + (s[i] - '0');
