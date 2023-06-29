751f0fdf5b2e7ba919f3468c4684537f732738b6
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 08:52:41 -0700

    3-main.c

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -9,13 +9,13 @@
  */
 void reverse_array(int *a, int n)
 {
-        int i;
-        int t;
+	int i;
+	int temp;
 
-        for (i = 0; i < n / 2; i++)
-        {
-                t = a[i];
-                a[i] = a[n - i - 1];
-                a[n - i - 1] = t;
-        }
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
 }
