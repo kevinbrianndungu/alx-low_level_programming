2abe3e27134c2772c5209a210e3cce56d4fdb036
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 15:09:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (indicating success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+    puts("Programming is like building a multilingual puzzle");
+
+    return 0;
 }
