79b6734df10a060ec4eeb683f9b7868a906e28e2
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 16:22:50 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+char *generate_password()
+{
+    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+    if (password == NULL)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    srand(time(NULL));
+
+    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = rand() % 94 + 33; // Generate random printable ASCII characters
+    }
+    password[PASSWORD_LENGTH] = '\0';
+
+    return password;
+}
+
+int main(void)
+{
+    char *password = generate_password();
+    printf("Generated Password: %s\n", password);
+    free(password);
+
+    return 0;
+}
