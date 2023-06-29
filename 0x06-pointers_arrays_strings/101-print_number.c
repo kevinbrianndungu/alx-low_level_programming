6226d31aef77a3382e918dd414eefbe6f5f88504
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 13:33:29 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,10 +2,9 @@
 
 /**
  * print_number - Prints an integer
- * @n: The integer to print
+ * @n: The integer to be printed
  *
- * Description: This function prints an integer using only the _putchar function.
- *              It handles negative numbers properly.
+ * Return: void
  */
 void print_number(int n)
 {
@@ -26,6 +25,5 @@ void print_number(int n)
 		print_number(num / 10);
 	}
 
-	_putchar((num % 10) + '0');
+	_putchar('0' + (num % 10));
 }
-
