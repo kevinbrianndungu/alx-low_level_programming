677ef9a53ebd4b82418eae5623c3442fa0e2b81e
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-06-29 15:36:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(message) - 1;
-    ssize_t written = write(2, message, len);
+	ssize_t len, written;
 
-    if (written == len)
-        return 1;
-    else
-        return -1;
+	len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+	written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+
+	if (written == len)
+		return (1);
+	else
+		return (-1);
 }
