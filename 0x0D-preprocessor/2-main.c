b5473f13a0af0743901d7a2c5a64e782847d1254
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-07-18 15:09:20 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    const char *filename = __FILE__;
-    int i = 0;
+	const char *filename = __FILE__;
+	int i = 0;
 
-    while (filename[i] != '\0')
-    {
-        putchar(filename[i]);
-        i++;
-    }
+	while (filename[i] != '\0')
+	{
+		putchar(filename[i]);
+		i++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return 0;
 }
