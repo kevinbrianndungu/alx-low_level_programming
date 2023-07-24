6fd91cb113c99acaf2e3a0390b295059e3763e13
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-07-24 00:23:34 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: The input string.
+ */
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        _putchar('\n'); // Print new line when we reach the end of the string
+        return;
+    }
+
+    _putchar(*s); // Print the current character
+    _puts_recursion(s + 1); // Recursively call the function for the next character
+}
