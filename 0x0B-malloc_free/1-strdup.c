6115fe567b623aad4e6837103fd2fc096f0df461
kevinbrianndungu <kevinbrianndungu>
2023-11-02 00:12:20 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,23 +9,23 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int length = 0;
-    unsigned int i;
+	char *duplicate;
+	unsigned int length = 0;
+	unsigned int i;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    while (str[length] != '\0')
-        length++;
+	while (str[length] != '\0')
+		length++;
 
-    duplicate = (char *)malloc((length + 1) * sizeof(char));
+	duplicate = (char *)malloc((length + 1) * sizeof(char));
 
-    if (duplicate == NULL)
-        return (NULL);
+	if (duplicate == NULL)
+		return (NULL);
 
-    for (i = 0; i <= length; i++)
-        duplicate[i] = str[i];
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
 
-    return (duplicate);
+	return (duplicate);
 }
