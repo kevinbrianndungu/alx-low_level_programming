5d6bb1f2c51f4c01850bd51499536e94d74cde3a
kevinbrianndungu <kevinbrianndungu>
2023-11-05 16:31:36 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,73 +1,30 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height)
-{
-    int **grid;
-    int h, w;
-
+int **alloc_grid(int width, int height) {
     if (width <= 0 || height <= 0) {
-        return NULL;
+        return NULL;  // Return NULL for invalid dimensions
     }
 
-    grid = (int **)malloc(sizeof(int *) * height);
-
+    int **grid = (int **)malloc(sizeof(int *) * height);
     if (grid == NULL) {
-        return NULL;
+        return NULL;  // Return NULL if malloc fails
     }
 
-    for (h = 0; h < height; h++) {
+    for (int h = 0; h < height; h++) {
         grid[h] = (int *)malloc(sizeof(int) * width);
         if (grid[h] == NULL) {
-            for (w = 0; w < h; w++) {
-                free(grid[w]);
+            // Free previously allocated memory
+            for (int i = 0; i < h; i++) {
+                free(grid[i]);
             }
             free(grid);
-            return NULL;
+            return NULL;  // Return NULL if malloc fails for a row
         }
-    }
 
-    for (h = 0; h < height; h++) {
-        for (w = 0; w < width; w++) {
+        for (int w = 0; w < width; w++) {
             grid[h][w] = 0;
         }
     }
 
     return grid;
-}
-
-void print_grid(int **grid, int width, int height)
-{
-    int h, w;
-
-    for (h = 0; h < height; h++) {
-        for (w = 0; w < width; w++) {
-            printf("%d ", grid[h][w]);
-        }
-        printf("\n");
-    }
-}
-
-int main(void)
-{
-    int **grid;
-    int h; /* Declare h here */
-
-    grid = alloc_grid(6, 4);
-
-    if (grid == NULL) {
-        return (1);
-    }
-
-    print_grid(grid, 6, 4);
-    printf("\n");
-
-    for (h = 0; h < 4; h++) {
-        free(grid[h]);
-    }
-
-    free(grid);
-
-    return (0);
 }
