79d63e08f1d767c39766cfb253c1942824eddb71
kevinbrianndungu <kevinbrianndungu>
2023-11-05 17:13:52 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,35 +9,34 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int h, w;
+	int **grid;  /* Pointer to the 2D grid */
+	int h, w, i;  /* Loop counters */
 
-    // Return NULL for invalid dimensions
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	/* Return NULL for invalid dimensions */
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    // Allocate memory for the array of pointers to rows
-    grid = (int **)malloc(sizeof(int *) * height);
+	/* Allocate memory for the array of pointers to rows */
+	grid = (int **)malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 
-    // If malloc fails, return NULL
-    if (grid == NULL)
-        return (NULL);
+	for (h = 0; h < height; h++)
+	{
+		/* Allocate memory for each row and initialize to 0 */
+		grid[h] = (int *)malloc(sizeof(int) * width);
+		if (grid[h] == NULL)
+		{
+			/* If malloc fails, free previously allocated memory and return NULL */
+			for (i = 0; i < h; i++)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
 
-    // Allocate memory for each row and initialize to 0
-    for (h = 0; h < height; h++)
-    {
-        grid[h] = (int *)malloc(sizeof(int) * width);
-        if (grid[h] == NULL)
-        {
-            // If malloc fails, free previously allocated memory and return NULL
-            for (w = 0; w < h; w++)
-                free(grid[w]);
-            free(grid);
-            return (NULL);
-        }
-        for (w = 0; w < width; w++)
-            grid[h][w] = 0;
-    }
+		for (w = 0; w < width; w++)
+			grid[h][w] = 0;  /* Initialize each element to 0 */
+	}
 
-    return (grid);
+	return (grid);
 }
