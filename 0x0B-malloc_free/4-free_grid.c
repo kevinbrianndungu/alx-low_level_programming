8c4b66ffe636172a07a1df46f7a93b98be05d9f6
kevinbrianndungu <kevinbrianndungu>
2023-11-05 17:40:32 -0800

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,17 +2,17 @@
 
 void free_grid(int **grid, int height)
 {
-    int h;  // Declare variable 'h' here
+    int h;  /* Declare variable 'h' here */
 
     if (grid == NULL || height <= 0) {
-        return;  // Nothing to free if grid is NULL or height is non-positive.
+        return;  /* Nothing to free if grid is NULL or height is non-positive. */
     }
 
     for (h = 0; h < height; h++) {
         if (grid[h] != NULL) {
-            free(grid[h]);  // Free each row of the grid.
+            free(grid[h]);  /* Free each row of the grid. */
         }
     }
 
-    free(grid);  // Free the grid itself.
+    free(grid);  /* Free the grid itself. */
 }
