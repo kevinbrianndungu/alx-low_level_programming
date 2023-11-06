62593b669eaece7505ab5a932ad408a2e1ca135d
kevinbrianndungu <kevinbrianndungu>
2023-11-05 16:34:17 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,27 +1,33 @@
 #include <stdlib.h>
 
 int **alloc_grid(int width, int height) {
+    int **grid;
+    int h, w, i;
+
+    /* Return NULL for invalid dimensions */
     if (width <= 0 || height <= 0) {
-        return NULL;  // Return NULL for invalid dimensions
+        return NULL;
     }
 
-    int **grid = (int **)malloc(sizeof(int *) * height);
+    /* Allocate memory for the rows of the 2D array */
+    grid = (int **)malloc(sizeof(int *) * height);
     if (grid == NULL) {
         return NULL;  // Return NULL if malloc fails
     }
 
-    for (int h = 0; h < height; h++) {
+    /* Allocate memory for each row and initialize elements to 0 */
+    for (h = 0; h < height; h++) {
         grid[h] = (int *)malloc(sizeof(int) * width);
         if (grid[h] == NULL) {
-            // Free previously allocated memory
-            for (int i = 0; i < h; i++) {
+            /* Free previously allocated memory to prevent leaks */
+            for (i = 0; i < h; i++) {
                 free(grid[i]);
             }
             free(grid);
             return NULL;  // Return NULL if malloc fails for a row
         }
 
-        for (int w = 0; w < width; w++) {
+        for (w = 0; w < width; w++) {
             grid[h][w] = 0;
         }
     }
