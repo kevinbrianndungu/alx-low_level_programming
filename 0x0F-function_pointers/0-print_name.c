02294cb050248f2ab61f11ec15b9728f692ccc98
kevinbrianndungu <kevinbrianndungu>
2023-11-11 02:46:53 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer to the printing function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+        f(name);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-0x0F-function_pointers
+this is a project on function_pointers
