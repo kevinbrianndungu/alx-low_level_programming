5373ce13ee4e9ab2e95bd96e5b2d64330cf7e25d
kevinbrianndungu <kevinbrianndungu>
2023-11-17 00:42:39 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 void _puts_recursion(char *s)
 {
     if (*s == '\0') {
-        putchar('\n');  // Print a new line when the end of the string is reached
+        putchar('\n');  /* Print a new line when the end of the string is reached */
         return;
     }
 
-    putchar(*s);  // Print the current character
-    _puts_recursion(s + 1);  // Recursively call the function with the next character
+    putchar(*s);  /* Print the current character */
+    _puts_recursion(s + 1);  /* Recursively call the function with the next character */
 }
 
 int main(void)
