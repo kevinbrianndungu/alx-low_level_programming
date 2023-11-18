425b88c37f0551f1c7b1f3622685942922343e76
kevinbrianndungu <kevinbrianndungu>
2023-11-18 10:23:13 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,21 +8,23 @@
  */
 int is_prime_number(int n)
 {
-    /* base cases */
     if (n <= 1)
         return 0;
-    if (n <= 3)
+
+    if (n == 2 || n == 3)
         return 1;
 
-    /* check for divisibility by 2 or 3 */
     if (n % 2 == 0 || n % 3 == 0)
         return 0;
 
-    /* check for prime numbers using 6k +/- 1 pattern */
-    for (int i = 5; i * i <= n; i += 6)
+    int i = 5;
+
+    while (i * i <= n)
     {
         if (n % i == 0 || n % (i + 2) == 0)
             return 0;
+
+        i += 6;
     }
 
     return 1;
