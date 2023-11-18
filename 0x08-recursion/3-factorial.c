7fdb4f03eb346cabe5d6cd609b949f1c5e843e84
kevinbrianndungu <kevinbrianndungu>
2023-11-18 05:18:00 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,9 +9,9 @@
 int factorial(int n)
 {
 	if (n < 0)
-		return -1; /* Return -1 to indicate an error for n < 0 */
+		return (-1); /* Return -1 to indicate an error for n < 0 */
 	else if (n == 0 || n == 1)
-		return 1; /* Factorial of 0 and 1 is 1 */
+		return (1); /* Factorial of 0 and 1 is 1 */
 	else
-		return n * factorial(n - 1); /* Recursive call for n * (n - 1)! */
+		return (n * factorial(n - 1)); /* Recursive call for n * (n - 1)! */
 }
