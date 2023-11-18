40939256c5e7779ec2aa413e7d11998f7d703b56
kevinbrianndungu <kevinbrianndungu>
2023-11-18 10:05:51 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,35 @@
 #include "main.h"
 
-int check_prime(int n, int i);
-
 /**
- * is_prime_number - Checks if a given number is a prime number.
- * @n: The input number.
- * 
- * Return: 1 if the number is prime, 0 otherwise.
+ * find_sqrt - Helper function to find the square root recursively.
+ * @n: The number to find the square root of.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
  */
-int is_prime_number(int n)
+int find_sqrt(int n, int guess)
 {
-    if (n <= 1)
-        return 0; /* 0 and 1 are not prime numbers */
-    return check_prime(n, 2);
+    if (guess * guess == n)
+        return guess;
+
+    if (guess * guess > n)
+        return -1;
+
+    return find_sqrt(n, guess + 1);
 }
 
 /**
- * check_prime - Helper function to check if a number is prime recursively.
- * @n: The input number.
- * @i: The current divisor to check.
- * 
- * Return: 1 if the number is prime, 0 otherwise.
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to find the square root of.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
  */
-int check_prime(int n, int i)
+int _sqrt_recursion(int n)
 {
-    if (i <= n / 2)
-    {
-        if (n % i == 0)
-            return 0; /* If n is divisible by i, it's not prime */
-        else
-            return check_prime(n, i + 1); /* Check the next divisor */
-    }
-    return 1; /* If no divisor found, n is prime */
+    if (n < 0)
+        return -1;
+
+    return find_sqrt(n, 0);
 }
